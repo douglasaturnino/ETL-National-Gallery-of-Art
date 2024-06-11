b4346cfb9247ae969e47f057fac59aec378ba411
Douglas Saturnino <douglasaturnino@gmail.com>
2024-06-11 11:11:56 -0300

    feat: Implementing html_collector with test and http_requester_mock

---
+++ src/drivers/html_collector_test.py
@@ -0,0 +1,13 @@
+from html_collector import HtmlCollector
+from mocks.http_requester_mock import mock_request_from_page
+
+
+def test_collect_essential_information():
+    http_request_response = mock_request_from_page()
+
+    html_collector = HtmlCollector()
+    collect_essential_information = html_collector.collect_essential_information(
+        http_request_response["html"]
+    )
+    print()
+    print(collect_essential_information)
