32d14eb96a8fa2e97260b0cf1996058747cf4f02
Douglas Saturnino <douglasaturnino@gmail.com>
2024-06-13 14:03:32 -0300

    feat: Implementing load stage

---
+++ src/stages/load/load_data.py
@@ -0,0 +1,17 @@
+from src.errors.load_error import LoadError
+from src.infra.interface.database_repository import DatabaseRepositoryInterface
+from src.stages.contracts.transform_contract import TransformContract
+
+
+class LoadData:
+    def __init__(self, repository: DatabaseRepositoryInterface) -> None:
+        self.__repository = repository
+
+    def load(self, transformed_data_contract: TransformContract) -> None:
+        try:
+            load_content = transformed_data_contract.load_content
+
+            for data in load_content:
+                self.__repository.insert_artist(data)
+        except Exception as exception:
+            raise LoadError(str(exception)) from exception
